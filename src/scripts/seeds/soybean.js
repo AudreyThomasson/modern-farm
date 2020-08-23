ca4f822f1ef0274627603671ce8b80a5976e8968
Audrey Thomasson <audrey@macbook-pro.lan>
2020-08-23 13:34:41 -0500

    have all the seeds

---
+++ src/scripts/seeds/soybean.js
@@ -0,0 +1,10 @@
+const seed = 
+    {   type: "soybean",
+        height: 20,
+        output: 4
+    }
+
+
+export const createSoybean = () => {
+    return seed;
+}
---
+++ src/scripts/seeds/sunflower.js
@@ -0,0 +1,10 @@
+const seed = 
+    {   type: "sunflower",
+        height: 380,
+        output: 3
+    }
+
+
+export const createSunflower = () => {
+    return seed;
+}
